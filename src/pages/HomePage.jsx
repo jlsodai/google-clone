4019dcca503c5d87d23b29cd7afd8614fac67629
Jules Maestro <jlsodai@gmail.com>
2022-03-10 18:38:49 +0000

    finalise homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer';
+import Search from '../components/Search';
 
 export const HomePage = () => {
   return (
-    <div>Home Page Goes Here</div>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <Search />
+      <Footer />
+    </div>
   )
 }
 

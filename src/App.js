902fb067eaa4685cc35aab06831a3548ea3d222b
Jules Maestro <jlsodai@gmail.com>
2022-03-11 04:45:12 +0000

    implement useContext for Search Context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React, { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 import './App.css';
+import { SearchContext } from "./appContext";
 import HomePage from './pages/HomePage';
 import SearchPage from "./pages/SearchPage";
 
 function App() {
+  const [search, setSearch] = useState('')
+
   return (
-    <div className="text-gray-800 text-[15px]">
-      <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="search" element={ <SearchPage />} />
-      </Routes>
-    </div>
+    <SearchContext.Provider value={{search, setSearch}}>
+      <div className="text-gray-800 text-[15px]">
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="search" element={ <SearchPage />} />
+        </Routes>
+      </div>
+    </SearchContext.Provider>
   );
 }
 
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useContext } from 'react';
+import { SearchContext } from '../appContext';
 
 const SearchPage = () => {
+  const {search, } = useContext(SearchContext)
   return (
-    <div>Search Page Goes Here</div>
+    <div>Search Page Goes { search }</div>
   )
 }
 

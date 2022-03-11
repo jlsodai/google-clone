4d4739fcd509d5f8f8a1d5d67be482a045276356
Jules Maestro <jlsodai@gmail.com>
2022-03-11 09:37:05 +0000

    initial build of SearchPage... include onSubmit, etc

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
-import { useContext } from 'react';
-import { SearchContext } from '../appContext';
+import { useState } from 'react';
+import SearchBar from '../components/SearchBar';
+import MenuIcon from '../components/MenuIcon';
+import Avatar from '../components/Avatar';
+import { Link, useSearchParams, useNavigate } from 'react-router-dom';
+import TopLinks from '../components/TopLinks';
 
 const SearchPage = () => {
-  const {search, } = useContext(SearchContext)
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchTerm, setSearchTerm] = useState(searchParams.get('s'))
+  const navigate = useNavigate();
+
+  const searched = (e) => {
+    e.preventDefault()
+    navigate('/search?s=' + searchTerm)
+  }
+
   return (
-    <div>Search Page Goes { search }</div>
+    <>
+      <div className="flex justify-between mx-6 mt-6 items-center">
+        <div className="flex shrink items-center space-x-4">
+          <img className="w-[86px] h-full mr-7" src="/images/logo_small.png" alt="" />
+          <form onSubmit={searched}>
+            <SearchBar onChange={e => setSearchTerm(e.target.value)} value={searchTerm} />
+          </form>
+        </div>
+        <div className="flex self-center space-x-2 items-center shrink-0">
+          <MenuIcon />
+          <Avatar />
+        </div>
+      </div>
+
+      <nav className="mt-3">
+        <TopLinks />
+      </nav>
+
+      <main className="container">
+        <div className="py-3">About 177,000,000 results (0.65 seconds)</div>
+      </main>
+
+      <div className="container">Results Section Goes Here</div>
+      <footer className="container">Footer Section Goes Here</footer>
+    </>
   )
 }
 

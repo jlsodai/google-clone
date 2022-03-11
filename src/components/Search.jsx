00449310d7292e5db726bfb744e0f7ac05f668e2
Jules Maestro <jlsodai@gmail.com>
2022-03-11 09:35:14 +0000

    refactor Search; introduce SearchBar component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { SearchContext } from '../appContext';
+import SearchBar from "./SearchBar";
 
 const Search = () => {
   const { setSearch } = useContext(SearchContext)
@@ -15,15 +16,9 @@ const Search = () => {
 
   return (
     <form className="flex flex-col pt-16 items-center grow space-y-6 text-[14px]">
-      <img src="/logo.png" className="w-[272px]" alt="logo" />
+      <img src="/images/logo.png" className="w-[272px]" alt="logo" />
 
-      <div className="relative">
-        <svg className="absolute w-4 bottom-4 left-4 stroke-gray-400" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-
-        <input type="text" className="border py-3 px-12 rounded-full w-[400px] md:w-[600px]" placeholder="Enter your search term here..." onChange={(e) => setSearchIntput(e.target.value)} autoFocus/>
-
-        <svg className="absolute right-5 w-6 top-3" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="#4285f4" d="m12 15c1.66 0 3-1.31 3-2.97v-7.02c0-1.66-1.34-3.01-3-3.01s-3 1.34-3 3.01v7.02c0 1.66 1.34 2.97 3 2.97z"></path><path fill="#34a853" d="m11 18.08h2v3.92h-2z"></path><path fill="#fbbc05" d="m7.05 16.87c-1.27-1.33-2.05-2.83-2.05-4.87h2c0 1.45 0.56 2.42 1.47 3.38v0.32l-1.15 1.18z"></path><path fill="#ea4335" d="m12 16.93a4.97 5.25 0 0 1 -3.54 -1.55l-1.41 1.49c1.26 1.34 3.02 2.13 4.95 2.13 3.87 0 6.99-2.92 6.99-7h-1.99c0 2.92-2.24 4.93-5 4.93z"></path></svg>
-      </div>
+      <SearchBar onChange={(e) => setSearchIntput(e.target.value)} />
 
       <div className="flex space-x-4">
         <button className="rounded bg-gray-100 px-4 py-2 hover:shadow-md" onClick={searched}>Google Search</button>

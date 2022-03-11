15ac6abbb74090874aafea8aed428bdf0779aed3
Jules Maestro <jlsodai@gmail.com>
2022-03-11 09:34:28 +0000

    refactor Header; create Avatar and MenuIcon components

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Avatar = () => {
+  return (
+    <img className="w-7 rounded-full" src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Avatar"/>
+  )
+}
+
+export default Avatar

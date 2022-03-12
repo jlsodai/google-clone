e17ea0fde6d31d7442dfce5e46c0b68b49dea8e4
Jules Maestro <jlsodai@gmail.com>
2022-03-12 05:42:42 +0000

    re configure error page to display static results

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,30 +1,36 @@
-import { useContext, useEffect, useState } from 'react'
-// import items from "../results.json";
+import { useState } from 'react'
+import staticItems from "../results.json";
 import SearchResult from './SingleResult';
-import { SearchContext } from '../appContext';
 import useGoogleSearch from '../useGoogleSearch';
 import { useSearchParams, Link } from 'react-router-dom';
 
 const SearchResults = () => {
-  // const { search } = useContext(SearchContext)
   const [searchParams] = useSearchParams();
   const [searchTerm] = useState(searchParams.get('s'))
-  const { items } = useGoogleSearch(searchTerm)
+  let { items } = useGoogleSearch(searchTerm)
+  let errorFetching = false
+
+  if (items.error) { errorFetching = true }
+  if (searchParams.get('static')) {
+    errorFetching = false
+    items = staticItems
+  }
 
   return (
-    <main className="container">
-      { items.error && <div>
-        <div className="flex min-h-[calc(100vh-260px)] mb-40 justify-center text-center flex-col">
-          <h1 className="text-[80px]">Check Again Tomorrow</h1>
-          <div className="max-w-lg mx-auto">{items.error.message}</div>
+    <main className="container min-h-[calc(100vh-260px)]">
+      { errorFetching && <div>
+        <div className="flex my-40 justify-center text-center flex-col">
+          <h1 className="text-[60px]">Search Quota Exceeded</h1>
+          <div className="max-w-lg mx-auto text-[20px]">Check again tomorrow or click the button below to view a static version of the search results.</div>
           <div className="mt-8 justify-center items-center sm:flex">
-            <Link to="" className="px-10 py-3.5 w-full bg-blue-600 text-white text-center rounded-md shadow-md block sm:w-auto">
-                View Static Results
+            <Link to="/search?s=Julius%20Odai&static=true" className="px-10 py-3.5 w-full bg-blue-600 text-white text-center rounded-md shadow-md block sm:w-auto">
+                Static Search Results
             </Link>
           </div>
         </div>
-      </div>}
-      { items > 0 && <>
+      </div> }
+
+      { !errorFetching && items && <>
         <div className="py-3">About {items.searchInformation.formattedTotalResults} ({items.searchInformation.formattedSearchTime} seconds)</div>
         <div className="max-w-[660px] mt-2">
           {items.items.map((item, i) => (
